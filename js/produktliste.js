3dbb96e697743622d09af187b91a96c83bd9cd96
mikkiatlaa <mikaelmaninemi@gmail.com>
2026-03-16 14:54:03 +0100

    Claude commit

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,31 +1,24 @@
 const productContainer = document.querySelector(".produktcards");
 const params = new URLSearchParams(window.location.search);
-const kategori = params.get("category");
-const season = params.get("season");
+const kategori = params.get("categories");
+const season = params.get("");
 
 console.log("season", season);
 
-let endpoint = "";
-if (kategori) {
-  endpoint = `https://kea-alt-del.dk/t7/api/products?category=${kategori}`;
-} else {
-  endpoint = `https://kea-alt-del.dk/t7/api/products?season=${season}`;
-}
-
-fetch(endpoint)
+fetch(`https://dummyjson.com/products/category/sunglasses`)
   .then((response) => response.json())
   .then((data) => {
     console.log("data", data);
     productContainer.innerHTML = "";
-    data.forEach((produkter) => {
+    data.products.forEach((produkter) => {
       productContainer.innerHTML += `
           <article class="produkt">
           <div class="produktbillede "> 
-            <a href="produkt.html?id=${produkter.id}"><img src="https://kea-alt-del.dk/t7/images/webp/640/${produkter.id}.webp" /></a>
-            <h2 class="produktnavn">${produkter.productdisplayname}</h2>
+            <a href="produkt.html?id=${produkter.id}"><img src="${produkter.thumbnail}" /></a>
+            <h2 class="produktnavn">${produkter.title}</h2>
           </div>
           <div class="info">
-            <p class="katagori">${produkter.brandname}: ${produkter.category}</p>
+            <p class="katagori">${produkter.brand}: ${produkter.category}</p>
             <p class="pris"><span class="valuta">DKK</span>${produkter.price},-</p>
           </div>
           <div>
@@ -35,32 +28,3 @@ fetch(endpoint)
         `;
     });
   });
-
-document.querySelectorAll(".buttons button").forEach((knap) => {
-  knap.addEventListener("click", filterButtonClick);
-
-  function filterButtonClick(evt) {
-    showFiltered(evt.currentTarget.dataset.gender);
-  }
-
-  function showFiltered(filter) {
-    let dataToShow;
-    if (filter === "all") {
-      dataToShow = allData;
-    } else {
-      dataToShow = allData.filter((produkt) => produkt.gender === filter);
-    }
-    showProducts(dataToShow);
-
-    let allData;
-    getData(`https://kea-alt-del.dk/t7/api/products?category=${category}`);
-    function getData(url) {
-      fetch(url).then((response) =>
-        response.json().then((data) => {
-          allData = data;
-          showProducts(allData);
-        }),
-      );
-    }
-  }
-});
